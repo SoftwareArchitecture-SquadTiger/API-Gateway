e49ffea9a12690268799781b513a6858700151b5
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-08 22:34:03 +0700

    delete demo cause I suck

--- demo/client/server.js
+++
@@ -1,8 +0,0 @@
-import host from '../host/host.js';
-
-const PORT = 4000;
-const HOST = "something";
-
-host.listen(PORT, '0.0.0.0',() => {
-    console.log(`Team A Application running on http://${HOST}:${PORT}`);
-});
