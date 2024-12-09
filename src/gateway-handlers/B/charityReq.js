5b6912bc1712a4dab308e83b810744a578918e0d
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-09 16:36:05 +0700

    add fetch charity by id method on the API Gateway

--- src/gateway-handlers/B/charityReq.js
+++ src/gateway-handlers/B/charityReq.js
@@ -5,6 +5,7 @@ const HOST = process.env.HOST;
 const PORT_B = process.env.TEAM_B_PORT;
 const TEAM_B_BASE_URL = `http://${HOST}:${PORT_B}`;
 
+//Get all charities
 export const getAllCharities = async (req, res, next) => {
     try {
         const response = await axios.get(`${TEAM_B_BASE_URL}/charity/all`);
@@ -19,8 +20,26 @@ export const getAllCharities = async (req, res, next) => {
     }
 };
 
+//Get charity by id
+export const getCharityById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const url = `${TEAM_B_BASE_URL}/charity/${id}`;
 
+        const response = await axios.get(url)
 
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error(`Error fetching Charity by id: ${error}`);
+
+        const status = error.response?.status || 500;
+        const message = error.response?.data?.message || error.message || 'Internal Server Error';
+
+        res.status(status).json({ error: message });
+    }
+};
+
+//Create a charity
 export const createNewCharity = async (req, res, next) => {
     try {
         const url = `${TEAM_B_BASE_URL}/charity/create`;
--- src/routes/teamB.routes.js
+++ src/routes/teamB.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllDonors } from '../gateway-handlers/B/donorReq.js';
-import { createNewCharity, getAllCharities } from '../gateway-handlers/B/charityReq.js';
+import { createNewCharity, getAllCharities, getCharityById } from '../gateway-handlers/B/charityReq.js';
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.get('/donors', getAllDonors);//GET all donors
 
 //Charity
 router.get('/charities', getAllCharities);//GET all charities
+router.get('/charity/:id',getCharityById);//GET charity by id
 router.post('/charity/create', createNewCharity);//POST new charity
 
 export default router;
