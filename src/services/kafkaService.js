a4605671e75fbc762a3db2f3cfb59cac7381b10f
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-16 17:57:16 +0700

    Update kafkaService.js

--- src/services/kafkaService.js
+++ src/services/kafkaService.js
@@ -15,7 +15,7 @@ export const sendKafkaMessage = async (topic, message) => {
         await producer.connect();
         await producer.send({
             topic: topic,
-            messages: [{ value: JSON.stringify(message) }];
+            messages: [{ value: JSON.stringify(message) }],
         });
         console.log(`Message sent to ${topic}`);
     } catch (error) {
