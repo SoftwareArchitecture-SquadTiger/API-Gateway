83c98f129022c4b4bd9e22a03e73fc920640991e
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-16 23:29:16 +0700

    Add new API for get all donors

--- src/gateway-handlers/B/donorReq.js
+++ src/gateway-handlers/B/donorReq.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "dotenv/config";
 import { handleAxiosErrorResponse } from "../../utils/errorHandler.js";
+import { sendKafkaMessageWithResponse } from "../../services/kafkaServices.js";
 
 const HOST = process.env.HOST;
 const PORT_B = process.env.TEAM_B_PORT;
@@ -9,10 +10,13 @@ const TEAM_B_BASE_URL = `http://${HOST}:${PORT_B}`;
 //Get all donors
 export const getAllDonors = async (req, res, next) => {
   try {
-    const response = await axios.get(`${TEAM_B_BASE_URL}/donor/all`);
-    res.status(200).json({ donorResponse: response.data });
+    const response = await sendKafkaMessageWithResponse('donor-request', {
+      action: 'GET_ALL',
+      data:{},
+    });
+    res.status(200).json(response);
   } catch (error) {
-    handleAxiosErrorResponse(error, res);
+    res.status(500).json({ error: error.message });
   }
 };
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import app from "./app.js";
+import { runKafkaResponseConsumer } from "./services/kafkaServices.js";
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`API Gateway running in http://${HOST}:${PORT}`);
+});
+
+runKafkaResponseConsumer("donor-response").catch((error) => {
+    console.error(`Error starting Kafka: ${error}`);
 });
 
 
