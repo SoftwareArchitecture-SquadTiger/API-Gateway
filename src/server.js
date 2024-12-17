28fa073bd6dbb07a71c59026a3566eb3396e04d5
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-18 01:50:43 +0700

    refactor for scaling

--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ import { runKafkaResponseConsumer } from "./services/kafkaServices.js";
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+const responseTopics = ["donor-response", "charity-response"]
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`API Gateway running in http://${HOST}:${PORT}`);
 });
 
-runKafkaResponseConsumer("donor-response").catch((error) => {
+runKafkaResponseConsumer(responseTopics).catch((error) => {
     console.error(`Error starting Kafka: ${error}`);
 });
 
