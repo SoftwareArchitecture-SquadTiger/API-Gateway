911fc7c4707029fbba212a0eb656ee2badfdc074
DatTran74 <67746863+DatTran74@users.noreply.github.com>
2024-12-17 09:30:48 +0700

    API update
    work good not work the date filter

--- src/gateway-handlers/A/projectReq.js
+++ src/gateway-handlers/A/projectReq.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import "dotenv/config";
 import { handleAxiosErrorResponse } from "../../utils/errorHandler.js";
+import e from "cors";
 
 const HOST = process.env.HOST;
 const PORT_A = process.env.TEAM_A_PORT;
 const TEAM_A_BASE_URL = `http://${HOST}:${PORT_A}`;
-
+//get
 export const getAllProjects = async (req, res, next) => {
     try {
         const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects`);
@@ -25,17 +26,159 @@ export const getProjectById = async (req, res, next) => {
         handleAxiosErrorResponse(error, res);        
     }
 };
+export const getProjectByCategory = async (req, res, next) => {
+    try {
+        const { category } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/category/${category}`);
 
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const  getCharityByID = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/charity/${id}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectByTargetAmountGte = async (req, res, next) => {
+    try {
+        const { targetAmount } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/target-amount/gte/${targetAmount}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectByTargetAmountLte = async (req, res, next) => {
+    try {
+        const { targetAmount } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/target-amount/lte/${targetAmount}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const sortProjectsByTargetAmountAsc = async (req, res, next) => {
+    try {
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/target-amount/asc`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const sortProjectsByTargetAmountDesc = async (req, res, next) => {
+    try {
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/target-amount/desc`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectsByCurrentAmountGte = async (req, res, next) => {
+    try {
+        const { currentAmount } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/current-amount/gte/${currentAmount}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectsByCurrentAmountLte = async (req, res, next) => {
+    try {
+        const { currentAmount } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/current-amount/lte/${currentAmount}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const sortProjectsByCurrentAmountAsc = async (req, res, next) => {
+    try {
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/current-amount/asc`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const sortProjectsByCurrentAmountDesc = async (req, res, next) => {
+    try {
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/current-amount/desc`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const filterProjectsByDate = async (req, res, next) => {
+    try {
+        const response = await axios.get(`${TEAM_A_BASE_URL}/from/${startDate}/to/${endDate}`);
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectsByCountry = async (req, res, next) => {
+    try {
+        const { country } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/country/${country}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+export const getProjectsByRegion = async (req, res, next) => {
+    try {
+        const { region } = req.params;
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/region/${region}`);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+}
+
+export const getProjectByStatus = async (req, res, next) => {
+    try {
+        console.log('Request received:', req.params);
+        const { status } = req.params;
+        console.log('Status:', status);
+
+        const response = await axios.get(`${TEAM_A_BASE_URL}/api/projects/status/${status}`);
+        console.log('Response received:', response);
+
+        res.status(response.status).json({ projectResponse: response.data });
+    } catch (error) {
+        console.error('Error occurred:', error);
+        handleAxiosErrorResponse(error, res);
+    }
+}
+
+//post
 export const createNewProject = async (req, res, next) => {
     try {
-        const response = await axios.post(`${TEAM_A_BASE_URL}/api/projects`);
+        const response = await axios.post(`${TEAM_A_BASE_URL}/api/projects`, req.body);
+
 
         res.status(response.status).json({ projectResponse: response.data });
     } catch (error) {
         handleAxiosErrorResponse(error, res);
     }
 };
-
+//put
 export const updateProjectById = async (req, res, next) => {
     try {
         const { id } = req.params;
@@ -43,6 +186,16 @@ export const updateProjectById = async (req, res, next) => {
 
         res.status(response.status).json({ projectResponse: response.data });
     } catch (error) {
+        handleAxiosErrorResponse(error, res);
+    }
+};
+//delete
+export const deleteProjectById = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const response = await axios.delete(`${TEAM_A_BASE_URL}/api/projects/${id}`);
+
+    }catch(error){
         handleAxiosErrorResponse(error, res);
     }
 };
--- src/routes/teamA.routes.js
+++ src/routes/teamA.routes.js
@@ -5,15 +5,45 @@ import {
   getAllProjects,
   getProjectById,
   updateProjectById,
+  deleteProjectById,
+  getProjectByStatus,
+  getProjectsByRegion,
+  getProjectsByCurrentAmountLte,
+  getProjectsByCurrentAmountGte,
+  getProjectsByCountry,
+  filterProjectsByDate,
+  sortProjectsByCurrentAmountDesc,
+  sortProjectsByCurrentAmountAsc,sortProjectsByTargetAmountAsc,sortProjectsByTargetAmountDesc
+  
+
+
+
+
+
 } from "../gateway-handlers/A/projectReq.js";
 
 const router = express.Router();
 
 //Project
-router.get("/projects", getAllProjects); //GET all projects
-router.get("/project/:id", getProjectById); //GET a project by id
-router.post("/project/create", createNewProject); //POST a project
-router.put("/project/update/:id", updateProjectById); //PUT a project by id
+//Get
+router.get("/projects", getAllProjects); //GET all projects(work)
+router.get("/project/:id", getProjectById); //GET a project by id(work)
+router.get("/project/status/:status", getProjectByStatus); //GET a project by status(work)
+router.get("/project/amount/lte/:currentAmount", getProjectsByCurrentAmountLte); //GET a project by current amount less than or equal to(work)
+router.get("/project/region/:region", getProjectsByRegion); //GET a project by region(work)
+router.get("/project/country/:country", getProjectsByCountry); //GET all projects sorted by target amount ascending(work)
+router.get("/project/date/:startDate/:endDate", filterProjectsByDate); //GET a project by date(not working)
+router.get("/project/amount/gte/:currentAmount", getProjectsByCurrentAmountGte); //GET a project by current amount greater than or equal to(work)
+router.get("/project/current-amount/asc", sortProjectsByCurrentAmountAsc); //GET all projects sorted by current amount ascending(work)
+router.get("/project/current-amount/desc", sortProjectsByCurrentAmountDesc); //GET all projects sorted by current amount descending(work)
+router.get("/project/target-amount/asc", sortProjectsByTargetAmountAsc); //GET all projects sorted by target amount ascending(work)
+router.get("/project/target-amount/desc", sortProjectsByTargetAmountDesc); //GET all projects sorted by target amount descending(work)
+//Post
+router.post("/project/create", createNewProject); //POST a project(work)
+//Put
+router.put("/project/update/:id", updateProjectById); //PUT a project by id (work)
+//Delete
+router.delete("/project/delete/:id", deleteProjectById); //DELETE a project by id
 
 
 export default router;
