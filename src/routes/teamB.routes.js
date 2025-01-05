78615165e6bffe461814257d46bbd12812b2f668
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-06 00:04:00 +0700

    add caching middleware

--- src/routes/teamB.routes.js
+++ src/routes/teamB.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { cacheMiddleware } from "../middlewares/cacheMiddleware.js";
+import { CACHE_KEYS } from "../utils/cacheKeys.js";
 
 import {
   createNewDonor,
@@ -29,10 +31,14 @@ import {
 const router = express.Router();
 
 //Donor
-router.get("/donors", getAllDonors); //GET all donors
+router.get(
+  "/donors",
+  cacheMiddleware(() => CACHE_KEYS.DONORS_ALL),
+  getAllDonors
+); //GET all donors
 router.get("/donor/id/:id", getDonorById); //GET donor by id
-router.get("/donors/subscribe/categories", getDonorsBySubscribedCategories) //GET donors by categories
-router.get("/donors/subscribe/regions", getDonorsBySubscribedRegions) //GET donors by regions
+router.get("/donors/subscribe/categories", getDonorsBySubscribedCategories); //GET donors by categories
+router.get("/donors/subscribe/regions", getDonorsBySubscribedRegions); //GET donors by regions
 router.post("/donor/create", createNewDonor); //POST a new donor
 router.put("/donor/update/:id", updateDonorById); //PUT a donor by id
 router.delete("/donor/delete/:id", deleteDonorById); //DELETE a donor by id
@@ -41,7 +47,7 @@ router.delete("/donor/delete/:id", deleteDonorById); //DELETE a donor by id
 router.get("/subscriptions/email/:email", getSubscriptionsByEmail); //GET regions & categories by email
 router.get("/subscriptions/emails/categories", getEmailsByCategories); //GET donors emails by categories
 router.post("/subscriptions/create", createSubscription); //POST a subscription
-router.put("/subscriptions/update/:email", updateSubscription); //PUT subscriptions by email 
+router.put("/subscriptions/update/:email", updateSubscription); //PUT subscriptions by email
 router.delete("/subscriptions/delete/:email", clearSubscription); //DELETE a subscriptions
 
 //Charity
