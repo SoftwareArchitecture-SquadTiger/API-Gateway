be623c67688fe33540f8c57eb4425472867ab1ae
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-06 01:04:04 +0700

    add logging for cache

--- src/middlewares/cacheMiddleware.js
+++ src/middlewares/cacheMiddleware.js
@@ -1,4 +1,5 @@
 import redisClient from "../services/redisService.js";
+import { logRedisMessage } from "../utils/redisLogHandler.js";
 
 export const cacheMiddleware = (key) => async (req, res, next) => {
   const cacheKey = key(req);
@@ -6,11 +7,12 @@ export const cacheMiddleware = (key) => async (req, res, next) => {
   try {
     const cachedData = await redisClient.get(cacheKey);
     if (cachedData) {
-      console.log(`Cache hit for key: ${cacheKey}`);
+      logRedisMessage("fetch", cacheKey);
       return res.json(JSON.parse(cachedData));
     }
 
     res.locals.cacheKey = cacheKey;
+    logRedisMessage("write", cacheKey);
     next();
   } catch (error) {
     console.error(`Error checking Redis cache: ${error}`);
