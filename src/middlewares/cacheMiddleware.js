029c1118ece836991167e0f65c5ca363075600b0
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-07 17:27:39 +0700

    rename log function

--- src/middlewares/cacheMiddleware.js
+++ src/middlewares/cacheMiddleware.js
@@ -1,5 +1,5 @@
 import redisClient from "../services/redisService.js";
-import { logRedisMessage } from "../utils/redisLogHandler.js";
+import { logCacheOperation } from "../utils/redisLogHandler.js";
 
 export const cacheMiddleware = (key) => async (req, res, next) => {
   if (!redisClient) {
@@ -12,12 +12,12 @@ export const cacheMiddleware = (key) => async (req, res, next) => {
   try {
     const cachedData = await redisClient.get(cacheKey);
     if (cachedData) {
-      logRedisMessage("fetch", cacheKey);
+      logCacheOperation("fetch", cacheKey);
       return res.json(JSON.parse(cachedData));
     }
 
     res.locals.cacheKey = cacheKey;
-    logRedisMessage("write", cacheKey);
+    logCacheOperation("write", cacheKey);
     next();
   } catch (error) {
     console.error(`Error checking Redis cache: ${error}`);
