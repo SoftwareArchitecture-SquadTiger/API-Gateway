4b602637abb955b818aba64a99ca414526e6599b
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-10 22:25:48 +0700

    add CORS for the 2 frontends

--- src/app.js
+++ src/app.js
@@ -1,22 +1,35 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 
-import teamBRoutes from './routes/teamB.routes.js';
-import teamARoutes from './routes/teamA.routes.js';
+import teamBRoutes from "./routes/teamB.routes.js";
+import teamARoutes from "./routes/teamA.routes.js";
 import { loggerMiddleware } from "./middlewares/loggerMiddleware.js";
 
 const app = express();
 
+const allowedOrigins = [
+  `http://${process.env.TEAM_A_FRONTEND_HOST}:${process.env.TEAM_A_FRONTEND_PORT}`,
+  `http://${process.env.TEAM_B_FRONTEND_HOST}:${process.env.TEAM_B_FRONTEND_PORT}`,
+];
 //Middleware
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true 
-  }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow cookies and authorization headers
+  })
+);
 app.use(express.json());
 app.use(loggerMiddleware);
 
 //Routes
-app.use('/admin-server', teamBRoutes);
-app.use('/client-server', teamARoutes);
+app.use("/admin-server", teamBRoutes);
+app.use("/client-server", teamARoutes);
 
 export default app;
