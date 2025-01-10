c65c407b6af4f1a110f5ae2c69b2f72ef3a2d99a
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-10 21:57:32 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,10 @@ import { loggerMiddleware } from "./middlewares/loggerMiddleware.js";
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    credentials: true 
+  }));
 app.use(express.json());
 app.use(loggerMiddleware);
 
